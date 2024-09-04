b7144da82da91f84b9e39f1d84db7e4ff1cc5810
Argchacal <ayala972@gmail.com>
2024-09-04 12:01:56 -0300

    agrege control y camara

--- Assets/script/motoScript.cs
+++ Assets/script/motoScript.cs
@@ -32,6 +32,32 @@ public class motoScript : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
+        grounded = Physics2D.OverlapCircle(traccionTrasera.transform.position,diametroRueda, suelo);
+        //rotacion como angulo  de euler en grado respecto al transformacion principal nos da un Vector3 usamos 
+        anguloMoto = transform.localEulerAngles.z;
+        Debug.Log("aguloMoto" + anguloMoto);
+        //corregimos el valor del angulo en negativo para que la lectura sea correcta
+        if(anguloMoto > 180)
+        {
+            anguloMoto = anguloMoto-360;
+        }
+        Debug.Log("aguloMoto arreglado" + anguloMoto);
+        if (grounded)
+        {
+            ruedaTrasera.motorSpeed = Mathf.Clamp(ruedaTrasera.motorSpeed - (aceleracion - gravedad * Mathf.PI* (anguloMoto/180)*80)*Input.GetAxis("Horizontal")*Time.deltaTime,velocidadMax,retrocesoMax);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ruedaTrasera.motorSpeed = 0;
+            }
+            ruedaDelantera = ruedaTrasera;
+            ruedaArticulacion[0].motor = ruedaTrasera;
+            ruedaArticulacion[1].motor = ruedaDelantera;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(traccionTrasera.transform.position, diametroRueda);
         
     }
 }
